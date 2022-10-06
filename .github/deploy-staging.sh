e2fb2807e0b500db6d93890c3591ccaf7198eaeb
Muspah <simondw@sapo.pt>
2022-10-06 18:08:16 +0200

    GH Actions: I always screw something up working with ci...

--- .github/deploy-staging.sh
+++ .github/deploy-staging.sh
@@ -5,11 +5,11 @@ echo -e "Publishing javadocs and artifacts...\n"
 cd $HOME
 
 rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
-$HOME/work/internetpolice-eu/BungeeAdminTools/target/mvn-repo/ \
+$HOME/work/BungeeAdminTools/BungeeAdminTools/target/mvn-repo/ \
 travis@travis.internetpolice.eu:WWW/repo/
 
 echo -e "Publishing javadocs...\n"
 
 rsync -r --delete --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" \
-$HOME/work/internetpolice-eu/BungeeAdminTools/target/site/apidocs/ \
+$HOME/work/BungeeAdminTools/BungeeAdminTools/target/site/apidocs/ \
 travis@travis.internetpolice.eu:WWW/javadocs/BungeeAdminTools/master/
